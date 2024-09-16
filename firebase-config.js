d28729fd259560fd9df86caa98ba35f588bb5e49
Joel1to96 <100208680+Joel1to96@users.noreply.github.com>
2024-09-16 15:38:32 +0100

    Update and rename config.js to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+const firebaseConfig = {
+            apiKey: "AIzaSyD2Ro-aX_BvF4FUQDYLqNOapc_0CZIh5Qg",
+            authDomain: "reastauranteapp.firebaseapp.com",
+            projectId: "reastauranteapp",
+            storageBucket: "reastauranteapp.appspot.com",
+            messagingSenderId: "15166663435",
+            appId: "1:15166663435:web:dbd9dadcb13e83a904500e",
+            measurementId: "G-H1N33YWYJ0"
+        };
+        
+        // Inicializar Firebase
+        firebase.initializeApp(firebaseConfig);
